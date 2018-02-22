cc7900a76e0f038faa9014c67847f53f9ed9bd86
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-21 23:11:04 -0800

    In the process of connecting golang server with postgres dataabse

--- example_server.go
+++ example_server.go
@@ -1,10 +1,27 @@
 package main
 
+
+// After these intial steps are countered we have to discuss the rest of the elements and how they factor into the
+// equation and we need to also import the postgres sql database package as well as the fmt package because we are going
+// to need the postgres sql package to open connections to the database and the fmt package to print out a string to
+// connect to the database and we will see how to to do that shortly
 import (
 	"net/http"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+// We are going to create the constant environmental variables we need to connect to our postgres database and what that
+// actually entails
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	db_name = "golang_example_database"
 )
 
 // First thing first we are essentially declaring a schema for how the data we are going to send and fetch is going to
@@ -37,5 +54,27 @@ func main() {
 	var router = mux.NewRouter()
 	sampleHumans = append(sampleHumans, Human{"Matthew", "Harrilal", 12})
 	router.HandleFunc("/showHumans",showSampleHumans).Methods("GET")
+
+	// Now at this point we are creating the connection string and this is basically interpolating all the credentials
+	// for the database and the neccesary format
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, db_name)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Succesfully connected")
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
+
+// Now the next step is to actually connect our server to the database
